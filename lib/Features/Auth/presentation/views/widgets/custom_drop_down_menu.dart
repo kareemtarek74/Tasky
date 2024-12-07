2456eb28538a434bebcaea7ca67ed6945a6e1078
kareemtarek74 <157656699+kareemtarek74@users.noreply.github.com>
2024-12-07 20:01:42 +0200

    add alert dialog in deleting tasks

--- lib/Features/Auth/presentation/views/widgets/custom_drop_down_menu.dart
+++ lib/Features/Auth/presentation/views/widgets/custom_drop_down_menu.dart
@@ -48,8 +48,50 @@ class CustomDropdown extends StatelessWidget {
               )),
           PopupMenuItem<String>(
             onTap: () {
-              BlocProvider.of<TaskCubit>(context).deleteTask(id: id);
-              BlocProvider.of<TaskCubit>(context).getTasksList();
+              Future.delayed(Duration.zero, () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text(
+                        "Confirm Delete",
+                        style: Styles.styleBold24(context),
+                      ),
+                      content: Text(
+                        "Are you sure you wanna delete this task?",
+                        style: Styles.styleMedium16(context)
+                            .copyWith(color: Colors.black),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(color: Color(0xFFFF7D53)),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            BlocProvider.of<TaskCubit>(context)
+                                .deleteTask(id: id);
+                            BlocProvider.of<TaskCubit>(context).getTasksList();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFFF7D53),
+                          ),
+                          child: const Text(
+                            "Delete",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              });
             },
             value: 'delete',
             padding: EdgeInsets.zero,
