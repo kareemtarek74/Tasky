a785d8f62e1aee33a89458bcf33b654a75c3182f
kareemtarek74 <157656699+kareemtarek74@users.noreply.github.com>
2024-12-09 04:02:55 +0200

    Removed TaskCubit from main.dart and provided it locally only to the screens that require it, improving state management efficiency and adhering to best practices for scoped Bloc usage.

--- lib/core/services/cubit_provider.dart
+++ lib/core/services/cubit_provider.dart
@@ -3,10 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tasky/Features/Auth/presentation/view_model/auth_cubit.dart';
 import 'package:tasky/core/services/get_it_service.dart';
 
-class AuthBlocProvider extends StatelessWidget {
+class AuthProvider extends StatelessWidget {
   final Widget child;
 
-  const AuthBlocProvider({super.key, required this.child});
+  const AuthProvider({super.key, required this.child});
 
   @override
   Widget build(BuildContext context) {
